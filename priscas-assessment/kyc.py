a43160892b7f5e8d6e70a8db5500c012c5ca3202
prisca-dev <priscilliachidi7@gmail.com>
2026-03-08 08:37:33 -0700

    finished kyc assessment

---
+++ priscas-assessment/kyc.py
@@ -0,0 +1,9 @@
+name = input("enter name: ")
+surname = input("enter surname: ")
+gender = input ("enter gender: ")
+age = input("enter age: ")
+tribe = ("enter tribe: ")
+occupation = input("enter occupation: ")
+print(f"your information is  \n Name: {name} \n Surname: {surname} \n Gender: {gender} \n Age: {age} \n Tribe: {tribe} \n Occupation: {occupation} ")
+# print(f"Name: {name}")
+

fa0bc8c6fef3a24e38e928a5c491c1c980fdcb19
prisca-dev <priscilliachidi7@gmail.com>
2026-03-10 05:26:13 -0700

    i l learnt about python data types

---
+++ challenge.py
@@ -0,0 +1,11 @@
+age = input("what is your age: ")
+height = input("what is your height: ")
+name = input("Enter your name: ")
+are_you_a_student = input("Yes or No:")
+something_with_no_value = input("")
+list_of_three_best_friend = input["Keren,Prisca,Dan:"]
+
+print(f"I am {age} years old")
+print(f"My age contains{age.bit_length()}")
+
+
---
+++ data_types.py
@@ -0,0 +1,20 @@
+a = "Hello"
+b = 'Emmanuel'
+c =  12
+d =  3.56 
+e = """138"""
+f = True
+g = [1,2,"Emily", True]
+h =None
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(f"This variable contains {len(g)} items")
+print(b.upper())
+print(b.lower())
+      

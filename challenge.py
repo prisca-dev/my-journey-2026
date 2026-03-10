485c7a38cc67a014d20d7c47cea15a3cd9446703
prisca-dev <priscilliachidi7@gmail.com>
2026-03-10 06:22:59 -0700

    i learnt how to use bit

--- challenge.py
+++ challenge.py
@@ -1,11 +1,21 @@
-age = input("what is your age: ")
-height = input("what is your height: ")
+age = int(input("what is your age: "))
+height = int(input("what is your height: "))
 name = input("Enter your name: ")
-are_you_a_student = input("Yes or No:")
+are_you_a_student = input("Are you a student (Yes or No): ")
 something_with_no_value = input("")
-list_of_three_best_friend = input["Keren,Prisca,Dan:"]
+list_of_three_best_friend = input("List of three best friend:")
 
 print(f"I am {age} years old")
 print(f"My age contains{age.bit_length()}")
+print()
+print(f"My height is {height} feet tall")
+print(f"My height contains{height.bit_length()}")
+print()
+print(f"My name is {name}")
+print()
+print(f"{are_you_a_student} I am a student")
+print()
+print(f"My best friends are {list_of_three_best_friend}")
+
 
 

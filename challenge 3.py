994b0fef6dcf003fa8810920c4d013968d46e1a2
prisca-dev <priscilliachidi7@gmail.com>
2026-03-17 03:15:12 -0700

    i learnt some stuffs

---
+++ challenge 3.py
@@ -0,0 +1,18 @@
+dirty = "968-Maria, ( D@t@ Engineering );; 27y.."
+clean = f"name:{dirty[4:9].lower()} | role:{dirty [12:26].lower().replace("@",'a')} | age:{dirty[31:33]}"
+print("*"*50)
+print(dirty)
+print("*"*50)
+print(clean)
+# phone1 = "+1-6567887464"
+# phone2 = "+234-9024343825"
+# phone3 = "+229-89888464646"
+# print(phone1[phone1.find("-"):].strip('-'))
+# print(phone2[phone2.find("-"):].strip("-"))
+# print(phone3[phone3.find("-"):].strip("-"))
+# email = "yomi@yahoo.com"
+
+# print("@" in email)
+# print("@" in email)
+
+
